1a345fe8873b4d6ed770b7208538bd21acc36476
zeeshanjunaid <zeeshanjunaid2222@gmail.com>
2020-03-27 07:00:51 +0500

    testing 1.5

--- src/components/header.js
+++ src/components/header.js
@@ -5,20 +5,7 @@ import React from "react"
 // import bannerBG from "../images/banner-bg.svg"
 
 const Header = () => {
-  return (
-    <header className="header-area">
-      {/* <Menu />
-      <div
-        id="home"
-        className="header-hero bg_cover"
-        style={{ backgroundImage: `url(${bannerBG})` }}
-      >
-        <Hero />
-        <ParticlesHeader />
-      </div> */}
-      header
-    </header>
-  )
+  return <>header here</>
 }
 
 export default Header
