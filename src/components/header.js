417d42c1b4c8a5197006e37361f04000b5cc3732
zeeshanjunaid <zeeshanjunaid2222@gmail.com>
2020-03-27 08:06:30 +0500

    testing

--- src/components/header.js
+++ src/components/header.js
@@ -7,14 +7,7 @@ import bannerBG from "../images/banner-bg.svg"
 const Header = () => {
   return (
     <header className="header-area">
-      <Menu />
-      <div
-        id="home"
-        className="header-hero bg_cover"
-        style={{ backgroundImage: `url(${bannerBG})` }}
-      >
-        <Hero />
-        <ParticlesHeader />
+     header here
       </div>
     </header>
   )
