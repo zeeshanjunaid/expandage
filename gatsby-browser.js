39596d7aba780d2fb429a4853804cdc8c53c9ea7
zeeshanjunaid <zeeshanjunaid2222@gmail.com>
2020-03-27 06:45:10 +0500

    testing

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,3 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./src/styles/default.css"
 import "./src/styles/animate.css"
--- src/components/layout.js
+++ src/components/layout.js
@@ -8,17 +8,17 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import Header from "./Header"
-import Footer from "./Footer"
 
-const Layout = ({ children }) => {
-  return (
-    <>
-      <Header />
-      <main>{children}</main>
-      <Footer />
-    </>
-  )
+class Layout extends React.Component {
+
+  render() {
+    const { children } = this.props
+    return (
+      <>
+        hello
+      </>
+    )
+  }
 }
 
 Layout.propTypes = {
