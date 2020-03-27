07aa2d020e093d581534c1912e5339441e639daf
zeeshanjunaid <zeeshanjunaid2222@gmail.com>
2020-03-27 07:14:29 +0500

    updated header

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 // import bannerBG from "../images/banner-bg.svg"
 
 const Header = () => {
-  return <>header here</>
+  return <h2>header here</h2>
 }
 
 export default Header
