e256d899fae023ef7081288aab4dec7f4b671161
zeeshanjunaid <zeeshanjunaid2222@gmail.com>
2020-03-28 13:30:11 +0500

    fully functionalo subscribe form

--- src/components/Subscribers.js
+++ src/components/Subscribers.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { Form, Input, Button } from "reactstrap"
+import { Form, Input, Button, Alert } from "reactstrap"
 
 const encode = data => {
   return Object.keys(data)
@@ -12,7 +12,11 @@ class Subscribers extends Component {
     super(props)
     this.state = {
       email: "",
+      success: false,
+      error: false,
     }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
   handleSubmit = e => {
     fetch("/", {
@@ -20,8 +24,8 @@ class Subscribers extends Component {
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ "form-name": "subscribers", ...this.state }),
     })
-      .then(() => alert("Success!"))
-      .catch(error => alert(error))
+      .then(() => this.setState({ success: true, email: "", error: false }))
+      .catch(error => this.setState({ error: true, email: "", success: false }))
 
     e.preventDefault()
   }
@@ -29,7 +33,7 @@ class Subscribers extends Component {
   handleChange = e => this.setState({ [e.target.name]: e.target.value })
 
   render() {
-    const { email } = this.state
+    const { email, error, success } = this.state
     return (
       <div className="subscribe-form mt-50">
         <Form
@@ -45,12 +49,23 @@ class Subscribers extends Component {
             onChange={this.handleChange}
             name="email"
             type="email"
+            required
             placeholder="Enter email"
           ></Input>
           <Button type="submit" className="main-btn">
             Subscribe
           </Button>
         </Form>
+        {success && (
+          <Alert color="success" className="mt-10">
+            You have successfully subscribed to our newsletter
+          </Alert>
+        )}
+        {error && (
+          <Alert color="danger" className="mt-10">
+            Something went wrong, please refresh the page and try again.
+          </Alert>
+        )}
       </div>
     )
   }
